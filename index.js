458f79833d0d93ee4b46e6b65eaaecf1077b279d
Ed Kubiak <ek33450505@gmail.com>
2022-04-03 21:37:52 -0400

    added questions to index

--- index.js
+++ index.js
@@ -1,29 +1,72 @@
-const fs = require('fs');
+//Include packages needed for this application
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// console.log(inquirer)
 
-const generatePage = require('./src/page-template.js');
+// Create an array of questions for user input
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the title of your project? (Required)'
+        },
+        {
+            type: 'input',
+            name: 'Description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type:'input',
+            name: 'Installation',
+            message: "Please describe how to insall your application (Required)"
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'Please explain how to use this application? (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'Licensing',
+            message: 'Which lisences are attached to this project? (Check all that apply)',
+            choices: ['Apache License 2.0', 'GNU General Public License', 'Boost Software License 1.0', 'Eclipse Public License', 'The Unlicense']
+        },
+        {
+            type: 'input',
+            name: 'Contribution Guidelines',
+            message: 'Please explain how someone can contribute to this application? (Required)'
+        },
+        {
+            type: 'input',
+            name: "Testing",
+            message: 'Explain how someone can test this project on their own. (Required)'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Please enter your GitHub username. (Required)'
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address. (Required)'
+        },
+        {
+            type: 'input',
+            name: 'Additional Information',
+            message: 'Please provide instructions on how to reach you with additional questions'
+        }
+    ]);
+};
+questions()
 
-const readmeDataArgs = process.argv.slice(2);
-
-const [title, github] = readmeDataArgs;
-
-
-
-  fs.writeFile('index.html', generatePage(title, github), err => {
-    if (err) throw err;
-  
-    console.log('README complete! Check out index.html to see the output!');
-  });
-
-  // // TODO: Include packages needed for this application
-
-// // TODO: Create an array of questions for user input
-// const questions = [];
-
-// // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
 
 // // TODO: Create a function to initialize app
 // function init() {}
 
-// // Function call to initialize app
+// // // Function call to initialize app
+// init();
+
+// // // Function call to initialize app
 // init();
