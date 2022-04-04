eb4cea91c877278b7cd2855e0129eb2072ab8685
Ed Kubiak <ek33450505@gmail.com>
2022-04-03 22:26:54 -0400

    Added validation to the questions

--- index.js
+++ index.js
@@ -8,54 +8,125 @@ const questions = () => {
     return inquirer.prompt([
         {
             type: 'input',
-            name: 'Title',
-            message: 'What is the title of your project? (Required)'
+            name: 'title',
+            message: 'What is the title of your project? (Required)',
+            validate: titleInput => {
+                if (titleInput) {
+                    return true;
+                } else {
+                    console.log('You need to enter a title to continue!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
-            name: 'Description',
-            message: 'Provide a description of the project (Required)'
+            name: 'description',
+            message: 'Provide a description of the project (Required)',
+            validate: descriptionInput => {
+                if (descriptionInput) {
+                    return true;
+                } else {
+                    console.log('Your need to enter a description of your project!');
+                    return false;
+                }
+            }
         },
         {
             type:'input',
-            name: 'Installation',
-            message: "Please describe how to insall your application (Required)"
+            name: 'installation',
+            message: "Please describe how to insall your application (Required)",
+            validate: installationInput => {
+                if (installationInput) {
+                    return true;
+                } else {
+                    console.log('You need to describe how to insall your application!')
+                }
+            }
         },
         {
             type: 'input',
             name: 'Usage',
-            message: 'Please explain how to use this application? (Required)'
+            message: 'Please explain how to use this application? (Required)',
+            validate: usageInput => {
+                if (usageInput) {
+                    return true;
+                } else {
+                    console.log('You need to explain how to use this application!')
+                }
+            }
         },
         {
             type: 'checkbox',
-            name: 'Licensing',
+            name: 'licensing',
             message: 'Which lisences are attached to this project? (Check all that apply)',
             choices: ['Apache License 2.0', 'GNU General Public License', 'Boost Software License 1.0', 'Eclipse Public License', 'The Unlicense']
         },
         {
             type: 'input',
-            name: 'Contribution Guidelines',
-            message: 'Please explain how someone can contribute to this application? (Required)'
+            name: 'contribution',
+            message: 'Please explain how someone can contribute to this project? (Required)',
+            validate: contributionInput => {
+                if (contributionInput) {
+                    return true;
+                } else {
+                    console.log('You must explain how someone can contribute to this porject!')
+                }
+            }
         },
         {
             type: 'input',
-            name: "Testing",
-            message: 'Explain how someone can test this project on their own. (Required)'
+            name: "testing",
+            message: 'Explain how someone can test this application on their own. (Required)',
+            validate: testingInput => {
+                if (testingInput) {
+                    return true;
+                } else {
+                    console.log('You must explain how to test this application!')
+                }
+            }
         },
         {
             type: 'input',
             name: 'github',
-            message: 'Please enter your GitHub username. (Required)'
+            message: 'Please enter your GitHub username. (Required)',
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log('Please provide your GitHub username!')
+                }
+            }
         },
         {
             type: 'input',
             name: 'email',
-            message: 'Please enter your email address. (Required)'
+            message: 'Please enter your email address. (Required)',
+            validate: emailInput => {
+                if (emailInput) {
+                    return true;
+                } else {
+                    console.log('Plese enter your email address!')
+                }
+            }
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAdditionalQuestions',
+            message: 'Would you like to provide instructions on how to reach you with additional questions',
+            default: true
         },
         {
             type: 'input',
-            name: 'Additional Information',
-            message: 'Please provide instructions on how to reach you with additional questions'
+            name: 'Additional Questions',
+            message: 'Please provide additional information on how to reach you with additional questions:',
+            when: ({ confirmAdditionalQuestions}) => {
+                if (confirmAdditionalQuestions) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
         }
     ]);
 };
